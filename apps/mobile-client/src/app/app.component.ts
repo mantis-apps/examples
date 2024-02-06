c6c38d9087ac9a09b11502ac3736a4ce85249eb2
Ryan Clements <ryanclementshax@gmail.com>
2024-02-06 14:46:46 -0500

    feat(mobile-client): Add ionic scaffold - Scaffold a basic ionic projecty

--- apps/mobile-client/src/app/app.component.ts
+++ apps/mobile-client/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'mantis-todo-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  title = 'mobile-client';
+  constructor() {}
 }
